0937ba8c55a59fd04316125ec4cedaeb3d1ac678
yoshani <yoshani.malinka@gmail.com>
2026-05-03 00:21:37 +1000

    fix unit tests

--- backend/api/main.py
+++ backend/api/main.py
@@ -1,3 +1,7 @@
+import os
+os.environ['OMP_NUM_THREADS'] = '1'
+os.environ['OPENBLAS_NUM_THREADS'] = '1'
+os.environ['XGB_NTHREAD'] = '1'
 from fastapi import FastAPI
 
 from backend.api.routes.api_routes import router
